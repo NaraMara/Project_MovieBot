4ea30e8328538f3a58de429ccda0c71e433ca45a
NaraMara <mrjogurt18@gmail.com>
2021-04-01 18:38:07 +0700

    Made a simple bot that replies to user with user's last message unless it's a standart command

---
+++ bot.js
@@ -0,0 +1,11 @@
+const { Telegraf, Telegram } = require('telegraf')
+const bot = new Telegraf('1684695082:AAFbWgsEGgvYeJUKDRuTEwszsfkHVtvAF0Q')  
+bot.start((ctx) => ctx.reply('Welcome'))  
+bot.help((ctx) => ctx.reply('Send me a sticker'))  
+bot.on('sticker', (ctx) => ctx.reply('sticker')) //bot.on это обработчик введенного юзером сообщения, в данном случае он отслеживает стикер, можно использовать обработчик текста или голосового сообщения
+bot.hears('hi', (ctx) => ctx.reply('Hey there')) // bot.hears это обработчик конкретного текста, данном случае это - "hi"
+bot.launch() // запуск бота
+bot.on('text', (ctx) => {
+   ctx.reply(ctx.message.text)
+   
+});
