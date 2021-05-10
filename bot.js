a5ae3c360b524b694192eec4322231095e1f0331
NaraMara <mrjogurt18@gmail.com>
2021-05-10 15:59:34 +0700

    Installation of nodes
    Installed all needed nodes in the project and checked if they are working correctly
    List of innodes :
    1. Cheerio
    2.Needle
    More info about nodes you can find at wiki
    https://science.pm.kreosoft.ru/projects/movie-app-2021/wiki

--- bot.js
+++ bot.js
@@ -1,4 +1,7 @@
 const { Telegraf, Telegram } = require('telegraf')
+var needle = require('needle');
+var cheerio = require('cheerio');
+
 const bot = new Telegraf('1684695082:AAFbWgsEGgvYeJUKDRuTEwszsfkHVtvAF0Q')  
 bot.start((ctx) => ctx.reply('Welcome'))  
 bot.help((ctx) => ctx.reply('Send me a sticker'))  
@@ -10,4 +13,12 @@ bot.on('text', (ctx) => {
    
 
 });
+needle.get('http://www.google.com', function(error, response) {
+  if (!error && response.statusCode == 200)
+    console.log("needle is working");
+});
+
+
+
+
 //test feature
